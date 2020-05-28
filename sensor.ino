37ed335b935e3f371ffa614c8e63175e9627b448
Thomas Obst <hpcthobs@hlrs.de>
2020-05-28 22:36:57 +0200

    reintroduced getDistancesParallel()

--- sensor.ino
+++ sensor.ino
@@ -176,3 +176,107 @@ void HCSR04SensorManager::getDistances() {
     delayMicroseconds(10);
   }
 }
+
+
+/*
+void HCSR04SensorManager::getDistancesParallel() {
+  const uint32_t max_timeout_us = clockCyclesToMicroseconds(UINT_MAX);
+  for (size_t idx = 0; idx < m_sensors.size(); ++idx)
+  {
+    m_sensors[idx].timeout_cycles = microsecondsToClockCycles(m_sensors[idx].timeout);
+    if (m_sensors[idx].timeout > max_timeout_us)
+    {
+      m_sensors[idx].timeout = max_timeout_us;
+      //Serial.write(" Setting timeout to max value");
+    }
+  }
+  for (size_t idx = 0; idx < m_sensors.size(); ++idx)
+  {
+    digitalWrite(m_sensors[idx].triggerPin, LOW);
+  }
+  delayMicroseconds(2);
+  noInterrupts();
+  for (size_t idx = 0; idx < m_sensors.size(); ++idx)
+  {
+    digitalWrite(m_sensors[idx].triggerPin, HIGH);
+    m_sensors[idx].awaitingEcho = false;
+    m_sensors[idx].echoBegins = false;
+    m_sensors[idx].echoReceived = false;
+    m_sensors[idx].duration = 0;
+  }
+  //Serial.write(" Sensors triggered ");
+  delayMicroseconds(10);
+  for (size_t idx = 0; idx < m_sensors.size(); ++idx)
+  {
+    digitalWrite(m_sensors[idx].triggerPin, LOW);
+    m_sensors[idx].start_cycle_count = xthal_get_ccount();
+  }
+  bool allEchoesIn = false;
+  bool state = HIGH;
+  while (!(allEchoesIn))
+  {
+    for (size_t idx = 0; idx < m_sensors.size(); ++idx)
+    {
+      if (xthal_get_ccount() - m_sensors[idx].start_cycle_count > m_sensors[idx].timeout_cycles)
+      {
+        m_sensors[idx].awaitingEcho = true;
+        m_sensors[idx].echoBegins = true;
+        m_sensors[idx].echoReceived = true;
+        m_sensors[idx].duration = 0;
+      }
+      if (m_sensors[idx].awaitingEcho == false)
+      {
+        if (digitalRead(m_sensors[idx].echoPin) != (state))
+        {
+          m_sensors[idx].awaitingEcho = true;
+          //Serial.write(" awaiting Echo ! ");
+        }
+      }
+      else if ( (m_sensors[idx].awaitingEcho == true) && (m_sensors[idx].echoBegins == false))
+      {
+        if (digitalRead(m_sensors[idx].echoPin) == (state))
+        {
+          m_sensors[idx].pulse_start_cycle_count = xthal_get_ccount();
+          m_sensors[idx].echoBegins = true;
+          //Serial.write(" Echo begins! ");
+        }
+      }
+      else if ( (m_sensors[idx].awaitingEcho == true) && (m_sensors[idx].echoBegins == true) && (m_sensors[idx].echoReceived == false) )
+      {
+        if (digitalRead(m_sensors[idx].echoPin) != (state))
+        {
+          m_sensors[idx].duration = clockCyclesToMicroseconds(xthal_get_ccount() - m_sensors[idx].pulse_start_cycle_count);
+          m_sensors[idx].distance = (m_sensors[idx].duration / 2) / 29.1; // Distanz in CM
+          float dist;
+          dist = m_sensors[idx].distance - float(m_sensors[idx].offset);
+          if ((dist > 0.0) && (dist < float(sensorValues[idx])))
+          {
+            sensorValues[idx] = uint8_t(dist);
+          }
+          m_sensors[idx].echoReceived = true;
+          //Serial.write(" Echo received! ");
+        }
+      }
+    }
+
+    allEchoesIn = true;
+    for (size_t idx = 0; idx < m_sensors.size(); ++idx)
+    {
+      allEchoesIn = allEchoesIn && m_sensors[idx].echoReceived;
+    }
+  }
+  for (size_t idx = 0; idx < m_sensors.size(); ++idx)
+  {
+    unsigned long currentTime = millis();
+    if(currentTime > m_sensors[idx].lastMinUpdate +1000)
+    {
+      m_sensors[idx].minDistance = MAX_SENSOR_VALUE;
+    }
+    if(sensorValues[idx] < m_sensors[idx].minDistance)
+    {
+      m_sensors[idx].minDistance = sensorValues[idx];
+      m_sensors[idx].lastMinUpdate = currentTime;
+    }
+  }
+}
+*/
