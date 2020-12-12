b26dac37018181805c36e7b67c22886e6e2f9eb0
amandel <andreas.mandel@gmail.com>
2020-12-12 13:56:52 +0100

    Remove unused methods

--- src/utils/file.cpp
+++ src/utils/file.cpp
@@ -19,87 +19,6 @@
 */
 #include "file.h"
 
-void FileUtil::listDir(fs::FS &fs, const char * dirname, uint8_t levels) {
-  Serial.printf("Listing directory: %s\n", dirname);
-
-  File root = fs.open(dirname);
-  if (!root) {
-    Serial.println("Failed to open directory");
-    return;
-  }
-  if (!root.isDirectory()) {
-    Serial.println("Not a directory");
-    return;
-  }
-
-  File file = root.openNextFile();
-  while (file) {
-    if (file.isDirectory()) {
-      Serial.print("  DIR : ");
-      Serial.println(file.name());
-      if (levels) {
-        listDir(fs, file.name(), levels - 1);
-      }
-    } else {
-      Serial.print("  FILE: ");
-      Serial.print(file.name());
-      Serial.print("  SIZE: ");
-      Serial.println(file.size());
-    }
-    file = root.openNextFile();
-  }
-}
-
-void FileUtil::createDir(fs::FS &fs, const char * path) {
-  Serial.printf("Creating Dir: %s\n", path);
-  if (fs.mkdir(path)) {
-    Serial.println("Dir created");
-  } else {
-    Serial.println("mkdir failed");
-  }
-}
-
-void FileUtil::removeDir(fs::FS &fs, const char * path) {
-  Serial.printf("Removing Dir: %s\n", path);
-  if (fs.rmdir(path)) {
-    Serial.println("Dir removed");
-  } else {
-    Serial.println("rmdir failed");
-  }
-}
-
-void FileUtil::readFile(fs::FS &fs, const char * path) {
-  Serial.printf("Reading file: %s\n", path);
-
-  File file = fs.open(path);
-  if (!file) {
-    Serial.println("Failed to open file for reading");
-    return;
-  }
-
-  Serial.print("Read from file: ");
-  while (file.available()) {
-    Serial.write(file.read());
-  }
-  file.close();
-}
-
-void FileUtil::writeFile(fs::FS &fs, const char * path, const char * message) {
-  Serial.printf("Writing file: %s\n", path);
-
-  File file = fs.open(path, FILE_WRITE);
-  if (!file) {
-    Serial.println("Failed to open file for writing");
-    return;
-  }
-  if (file.print(message)) {
-    Serial.println("File written");
-  } else {
-    Serial.println("Write failed");
-  }
-  file.close();
-}
-
 bool FileUtil::appendFile(fs::FS &fs, const char * path, const char * message) {
   bool result = false;
   Serial.printf("Appending to file: %s\n", path);
@@ -117,18 +36,4 @@ bool FileUtil::appendFile(fs::FS &fs, const char * path, const char * message) {
   }
   file.close();
   return result;
-}
-
-bool FileUtil::renameFile(fs::FS &fs, const char * path1, const char * path2) {
-  Serial.printf("Renaming file %s to %s\n", path1, path2);
-  return fs.rename(path1, path2);
-}
-
-void FileUtil::deleteFile(fs::FS &fs, const char * path) {
-  Serial.printf("Deleting file: %s\n", path);
-  if (fs.remove(path)) {
-    Serial.println("File deleted");
-  } else {
-    Serial.println("Delete failed");
-  }
 }
--- src/utils/file.h
+++ src/utils/file.h
@@ -24,14 +24,7 @@
 
 class FileUtil {
   public:
-    static void listDir(fs::FS &fs, const char * dirname, uint8_t levels);
-    static void createDir(fs::FS &fs, const char * path);
-    static void removeDir(fs::FS &fs, const char * path);
-    static void readFile(fs::FS &fs, const char * path);
-    static void writeFile(fs::FS &fs, const char * path, const char * message);
     static bool appendFile(fs::FS &fs, const char * path, const char * message);
-    static bool renameFile(fs::FS &fs, const char * path1, const char * path2);
-    static void deleteFile(fs::FS &fs, const char * path);
 };
 
 #endif //OPENBIKESENSORFIRMWARE_FILE_H
