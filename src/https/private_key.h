c4ac92e7888ffe64e536fb24c36116bc851d9eaa
Andreas Mandel <andreas.mandel@gmail.com>
2021-03-25 20:01:19 +0100

    Fix and align copyright header LGPL

--- src/https/private_key.h
+++ src/https/private_key.h
@@ -1,3 +1,26 @@
+/*
+ * Copyright (C) 2019-2021 OpenBikeSensor Contributors
+ * Contact: https://openbikesensor.org
+ *
+ * This file is part of the OpenBikeSensor sensor firmware.
+ *
+ * The OpenBikeSensor sensor firmware is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * OpenBikeSensor sensor firmware is distributed in the hope that
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with the OpenBikeSensor sensor firmware.  If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef PRIVATE_KEY_H_
 #define PRIVATE_KEY_H_
 unsigned char obs_key_DER[] = {
